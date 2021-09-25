884af99d5589c5bd0185b79ee3e7f076c4ebd05b
DESKTOP-IA02F8S\Matheus <git-commit-address>
2021-09-25 20:02:46 -0300

    fix app erro

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 require('module-alias/register')
 
-
-
+const config = require('@config')
+const app = require('@app')
 
 const mongoose = require('mongoose')
 
@@ -14,8 +14,6 @@ const server = http.createServer(app)
 mongoose
     .connect(config.db.uri, { useNewUrlParser: true })
     .then(() => {
-        const config = require('@config')
-        const app = require('@app')
         const port = config.app.port        
         server.listen(port, ()=>{
             console.log(`Servidor inicializado ->  http://localhost:${port}`)
