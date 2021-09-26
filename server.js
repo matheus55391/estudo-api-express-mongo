52c599702af7e51b515928a9e0d70d8e0680859c
DESKTOP-IA02F8S\Matheus <git-commit-address>
2021-09-26 16:49:31 -0300

    chamada do mongoose movida do server.js para o app.js

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 require('dotenv').config()
 require('module-alias/register')
-
+const http = require('http')
 const config = require('@config')
 const app = require('@app')
+const port = config.app.port    
 
-const mongoose = require('mongoose')
-
-const http = require('http')
 const server = http.createServer(app)
 
-
-
-mongoose
-    .connect(config.db.uri, { useNewUrlParser: true })
-    .then(() => {
-        const port = config.app.port        
-        server.listen(port, ()=>{
-            console.log(`Servidor inicializado ->  http://localhost:${port}`)
-        })        
-    })
+server.listen(port, ()=>{
+    console.log(`Servidor inicializado ->  http://localhost:${port}`)
+})
--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
+const config = require('@config')
 const express = require("express")
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+
 const app = express()
+
+mongoose.connect(config.db.uri, { useNewUrlParser: true })
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(morgan('dev'))
 
 const routes = require('./routes')
-const { use } = require("./routes")
+
 
 app.use('/api', routes)
 
